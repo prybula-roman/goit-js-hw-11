80037fce995b19d58056f09f3e953e4fef06238f
roman prybula <roman.bismail@gmail.com>
2021-12-20 23:29:46 +0300

    test

--- index.js
+++ index.js
@@ -9,23 +9,38 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
+const list = document.querySelector(".gallery-list");
+var lightbox = new SimpleLightbox('.gallery .list-item .refs');
+
+let rezView="";
+
+
+let count_click=0;
 
 const opt = {
   key: "24814547-7669f4452a14656066293be0d",
   q: "",
 
-  // per_page: 0,
-  //  page: 0,
+   per_page: 10,
+    page: 1,
 };
 
 btn.addEventListener("click", () => {
-  // opt.per_page += 12;
-  //opt.page++;
-  opt.q = input.value;
-  const searchParams = new URLSearchParams(opt);
+ 
+  if(opt.q.trim()!=input.value.trim()){
 
-  console.log("searchParams.toString=  ", searchParams.toString());
+        rezView="";
+        opt.q = input.value;
+        opt.page=1;    
+       }
 
+if(opt.q.trim()===input.value.trim()){
+    opt.page++;
+
+       }
+       
+       const searchParams = new URLSearchParams(opt);
+  //console.log("searchParams.toString=  ", searchParams.toString());
   baseUrl = `https://pixabay.com/api/?${searchParams.toString()}`;
   console.log("********", input.value);
   console.log("baseUrl=", baseUrl);
@@ -33,7 +48,33 @@ btn.addEventListener("click", () => {
   fetch(baseUrl)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(data);
+      const markup = data.hits;
+      console.log("markup=",markup);
+
+      rezView+= markup.map((elem) => `<li class="list-item"><a rel=\"lightbox\" class="refs" href=\"${elem.largeImageURL}\"><img src=\"${elem.previewURL}\"></li>`)
+      .join("");
+      console.log("rezView=",rezView);
+      list.innerHTML = rezView;
+
+      //var lightbox = new SimpleLightbox('.gallery .list-item .refs');
+      var lightbox = new SimpleLightbox('.gallery  .refs');
+console.dir(btn)
+
+
+//======================================
+
+
+// let gallery = new SimpleLightbox('.gallery-list a');
+// gallery.on('show.simplelightbox', function () {
+// 	console.log("e.target=" ,e.target)
+// });
+
+//======================================
+      list.addEventListener("click",(e)=>{
+        
+
+        console.log("e.target=" ,e.target)
+      })
       //   SimpleLightbox.open({
       //     // items: ['demo/images/1big.jpg', 'demo/images/2big.jpg', 'demo/images/3big.jpg']
       // });
@@ -44,14 +85,14 @@ btn.addEventListener("click", () => {
 });
 //==================================================
 
-const list = document.querySelector(".gallery-list");
+// const list = document.querySelector(".gallery-list");
 
-const markup = galleryItems
-  .map((elem) => `<li class="list-item"><img src=\"${elem.preview}\"></li>`)
-  .join("");
+// const markup = galleryItems
+//   .map((elem) => `<li class="list-item"><img src=\"${elem.preview}\"></li>`)
+//   .join("");
 
-console.log("markup", markup);
-list.innerHTML = markup;
-
+// console.log("markup", markup);
+// list.innerHTML = markup;
+//===================================================
 
 
