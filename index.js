d7c89632d70a8fb5cc190cff0ded08fab055757c
roman prybula <roman.bismail@gmail.com>
2021-12-20 18:49:39 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 import Notiflix from "notiflix";
-//import  templ  from "./templates/markup.hbs";
+// import  templ  from "./templates/markup.hbs";
 import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -11,27 +11,34 @@ const btn = document.querySelector(".btn");
 const opt = {
   key: "24814547-7669f4452a14656066293be0d",
   q: "",
-  per_page: 0,
-  //  page: 0,
-};
+}
 
-const searchParams = new URLSearchParams(opt);
-console.log(searchParams.toString());
 
-//const baseUrl = `https://pixabay.com/api/?${searchParams.toString()}`;
-btn.addEventListener("click", () => {
-  opt.per_page += 12;
-  //opt.page++;
-  opt.q = input.value;
+btn.addEventListener("click",()=>{
+const query = input.value;
+render(query);
+})
+
+const render=(query)=>{ 
+  opt.q=query;
+
   const searchParams = new URLSearchParams(opt);
-
+  console.log("searchParams=",searchParams);
   baseUrl = `https://pixabay.com/api/?${searchParams.toString()}`;
-  console.log("********", input.value);
-  console.log("baseUrl=", baseUrl);
-  fetch(baseUrl)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch();
-});
+  console.log("baseUrl=",baseUrl);
+   fetch(baseUrl)
+   .then((resp) => resp.json())
+   .then((data) => {
+     console.log(data.per_page);
+   }).catch();
+
+}
+
+
+
+
+
+
+
+
+
